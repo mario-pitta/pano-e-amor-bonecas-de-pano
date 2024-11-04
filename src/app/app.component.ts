da5d96ad370f2eabd49a5de87bc608a1f30f1a25
Mario Pitta <pitta_mario@hotmail.com>
2024-11-04 10:50:16 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pano-e-amor-bonecas-de-pano';
+}
